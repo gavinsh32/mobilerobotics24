2ce48d74d6bd3195b0f0a4ea5d29de12c245b317
Gavin <gavinhaynes32@gmail.com>
2024-04-12 15:41:43 -0700

    Added Shutdown Sequence and More
    close all connections on exit, rearranged main loop and added more comments

--- pico/server.py
+++ pico/server.py
@@ -2,28 +2,38 @@ import network
 import socket             # set up a data connection with the client
 from machine import Pin   # pinout interface
 
+# init access point ====================================================================
 serverName = 'Drone Brain'
 password = 'mobile24'
-
-rled = machine.Pin(14, Pin.OUT)
-bled = machine.Pin(15, Pin.OUT)
-
-# create a network on the pico
-ap = network.WLAN(network.AP_IF)                      # create an access point network
-
-ap.config(essid=serverName, password=password)        # set name and 
-ap.active(True)                                       # turn on
+ap = network.WLAN(network.AP_IF)                      	# create an access point network
+ap.config(essid=serverName, password=password)        	# set name and 
+ap.active(True)
 print('Successfuly created network: %s' % serverName)
 print(ap.ifconfig())
+# ======================================================================================
 
-# create a socket interface
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   #creating socket object
-sock.bind(('', 800))
-sock.listen(1)
+# init led pins
+rled = machine.Pin(14, Pin.OUT)
+bled = machine.Pin(15, Pin.OUT)
+                                    
+# create a socket interface ============================================================
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)	# creating socket object
+sock.bind(('', 800)) 	# bind (connect) sock to port 800 on this pico's ip address
+sock.listen(1)			# (the first value returned from ap.ifconfig())
+# ======================================================================================
 
-# main loop
+conn, addr = sock.accept() 			
+print('Got a connection from %s' % str(addr))
+    
 while True:
-    conn, addr = sock.accept()
-    print('Got a connection from %s' % str(addr))
-    request = str(conn.recv(1024))
+    # recieve data from the socket
+    request = conn.recv(1024)        
+    if request == b'':
+        break
     print('Content = %s' % request)
+        
+# cleanup
+conn.close()
+sock.close()
+ap.active(False)
+print("Exit Successful")
