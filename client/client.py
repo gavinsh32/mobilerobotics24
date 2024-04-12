fc773313d04bb35479559367334f4c13c49137bc
Gavin <gavinhaynes32@gmail.com>
2024-04-12 15:42:56 -0700

    Update client.py
    Testing send data to

--- client/client.py
+++ client/client.py
@@ -6,8 +6,10 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect(('192.168.4.1', 800))
 
 def main():
-	while True:
-		sock.sendall('hello, pico!'.encode('utf-8'))
+	sock.sendall('hello, pico!'.encode())
+	sock.sendall('hello, pico!'.encode())
+	sock.sendall('hello, pico!'.encode())
+	sock.close()
 
 if __name__ == '__main__':
 	main()
