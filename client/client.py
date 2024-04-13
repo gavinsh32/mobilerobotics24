f8896dc6c22a498325df8499b77ae3db49ee9a98
Gavin <gavinhaynes32@gmail.com>
2024-04-12 17:21:36 -0700

    Removed Encoding flag

--- client/client.py
+++ client/client.py
@@ -6,9 +6,9 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect(('192.168.4.1', 800))
 
 def main():
-	sock.sendall('hello, pico!'.encode())
-	sock.sendall('hello, pico!'.encode())
-	sock.sendall('hello, pico!'.encode())
+	sock.sendall('hello, pico!')
+	sock.sendall('hello, pico!')
+	sock.sendall('hello, pico!')
 	sock.close()
 
 if __name__ == '__main__':
