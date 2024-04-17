4fa7c56c4f2c987ebe57d495bb5b0bc5ca71d397
Gavin <gavinhaynes32@gmail.com>
2024-04-17 16:05:06 -0700

    Update client.py
    Switched if / else logic

--- client/client.py
+++ client/client.py
@@ -4,16 +4,27 @@ import socket
 import sys
 
 def main():
-	if len(sys.argv) == 1:
-		print("To run this program: client.py xxx.xxx.x.x")
-		exit()
-	else: 
+	
+	if len(sys.argv) == 2:
+		# create a socket connected to Pico IP address (sys.argv[1])
 		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		sock.connect((sys.argv[1], 800))
+
+		# send three messages
+		# b indicates 'binary literal'
+		# sendall splits the total data up in to
+		# seperate packets if larger
 		sock.sendall(b'hello, pico!')
 		sock.sendall(b'hello, pico!')
 		sock.sendall(b'hello, pico!')
+		
+		# close socket and connection
 		sock.close()
+
+	# incorrect run paramaters
+	else: 
+		print("To run this program: python3 client.py xxx.xxx.x.x")
+		exit()
 
 if __name__ == '__main__':
 	main()
